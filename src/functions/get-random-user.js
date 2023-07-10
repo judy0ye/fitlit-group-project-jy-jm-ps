d0ccdb31bd0c6253a1ff80002a9a13e249396d1c
Parvin Sattorova <nanajon66@gmail.com>
2023-07-10 17:57:47 -0600

    feat: Add getRandomUser function and tests, UserRepository-test edit, and refine pull request template

--- src/functions/get-random-user.js
+++ src/functions/get-random-user.js
@@ -1,12 +1,8 @@
 function getRandomUser(users) {
     const randomIndex = Math.floor(Math.random() * users.length);
-    console.log(users); 
-const user1 = getRandomUser(users);
-
-
     return users[randomIndex];
-    }
+}
 
-    export {
-        getRandomUser,
-    }
+export {
+    getRandomUser,
+}
--- test/get-random-user-test.js
+++ test/get-random-user-test.js
@@ -1,24 +1,49 @@
 import { expect } from 'chai';
 import { getRandomUser } from '../src/functions/get-random-user';
-//import { users } from '../src/data/users'; // replace with the path to your users data
-import users from '../src/data/users'; 
 
-describe('getRandomUser function', () => {
-    it('should return a user from the users array', function () {
-        const user = getRandomUser(users);
-        const userIds = users.map(user => user.id);
-        console.log(user); 
-        console.log(userIds);
-        expect(userIds).to.include(user.id);
-    });
-      
+describe('getRandomUser function', function () {
+  let users;
 
-  it.skip('should return different users on successive calls', function () {
-    const user1 = getRandomUser(users);
-    const user2 = getRandomUser(users);
-    const user3 = getRandomUser(users);
+  beforeEach(() => {
+    users = [
+      {
+        id: 2,
+        name: 'Tyreek VonRueden',
+        address: '623 Koelpin Skyway, Lake Luigichester MN 77576-1678',
+        email: 'Nicolette_Halvorson43@yahoo.com',
+        strideLength: 4.5,
+        dailyStepGoal: 9000,
+        friends: [13, 19, 3]
+      },
+      {
+        id: 3,
+        name: 'Colt Rohan',
+        address: '48010 Balistreri Harbor, Cleobury IN 43317',
+        email: 'Wilford.Barton@gmail.com',
+        strideLength: 2.7,
+        dailyStepGoal: 3000,
+        friends: [31, 16, 15, 7]
+      },
+      {
+        id: 4,
+        name: 'Evie Satterfield',
+        address: '1378 Renner Island, Port Lincoln NE 06237-3602',
+        email: 'Adan66@yahoo.com',
+        strideLength: 3.9,
+        dailyStepGoal: 4000,
+        friends: [21, 32, 8]
+      }
+    ];
+  });
 
-    expect(user1.id).to.not.equal(user2.id);
-    expect(user2.id).to.not.equal(user3.id);
+  it('should return a user from the users array', function () {
+    const user = getRandomUser(users);
+    const userIds = users.map(user => user.id);
+    expect(userIds).to.include(user.id);
+  });
+
+  it('should return undefined if the users array is empty', function () {
+    const user = getRandomUser([]);
+    expect(user).to.be.undefined;
   });
 });
