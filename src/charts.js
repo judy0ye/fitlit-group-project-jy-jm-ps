1b7f13ca557abd0bb5450fa8f2d19512d91ad2e1
Parvin Sattorova <nanajon66@gmail.com>
2023-07-27 18:22:34 -0600

    feat: Add chart function for activity

--- src/charts.js
+++ src/charts.js
@@ -1,7 +1,11 @@
 /* ~~~~~~~~~~ CHARTS ~~~~~~~~~~*/
 
-import {getWeeklyHydration, getWeeklyInfo, getWeeklySleep}from './domUpdates';
+import {getWeeklyHydration, 
+  getWeeklyInfo, 
+  getWeeklySleep
+}from './domUpdates';
 import { Chart, LineController, LinearScale, PointElement, LineElement } from 'chart.js';
+import { currentUser } from './scripts';
 
 Chart.register(LineController, LinearScale, PointElement, LineElement);
 
@@ -104,42 +108,50 @@ function createSleepChart(sleepData) {
 }
 
 
-// function displaySleepChart(sleep, currentUser) {
-//   let avgSleep = getAvgSleep(sleep, currentUser.id);
-//   let avgQuality = getAvgQuality(sleep, currentUser.id);
+document.querySelector('.activity-button').addEventListener('click', () => {
+  let activityData = getWeeklyInfo('activity'); 
+  createActivityChart(activityData);
+});
 
-//   // Get a reference to the canvas element
-//   let sleepChartContext = document.getElementById('myChart').getContext('2d');
+function createActivityChart(activityData) {
+  const activityChartContext = document.getElementById('activityChart').getContext('2d');
 
-//   // Create the chart
-//   let sleepChart = new Chart(sleepChartContext, {
-//     type: 'bar',
-//     data: {
-//       labels: ['Avg Sleep', 'Avg Quality'],
-//       datasets: [
-//         {
-//           label: 'Hours / Rating',
-//           data: [avgSleep, avgQuality],
-//           backgroundColor: [
-//             'rgba(75, 192, 192, 0.2)',
-//             'rgba(255, 206, 86, 0.2)',
-//           ],
-//           borderColor: ['rgba(75, 192, 192, 1)', 'rgba(255, 206, 86, 1)'],
-//           borderWidth: 1,
-//         },
-//       ],
-//     },
-//     options: {
-//       responsive: true,
-//       scales: {
-//         y: {
-//           beginAtZero: true,
-//         },
-//       },
-//     },
-//   });
-// }
+  const labels = activityData.map((entry) => entry.date);
+  const data = activityData.map((entry) => entry.numSteps);
+  const backgroundColors = activityData.map((entry) =>
+    entry.numSteps >= currentUser.dailyStepGoal ? 'rgba(0, 123, 255, 0.6)' : 'rgba(255, 99, 132, 0.6)' // Blue for goal met, red for goal not met
+  );
 
+  const activityChart = new Chart(activityChartContext, {
+    type: 'bar',
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          label: 'Steps',
+          data: data,
+          backgroundColor: backgroundColors,
+          borderColor: backgroundColors.map((color) => color.replace('0.6', '1')), // Use a darker color for the border
+          borderWidth: 2,
+        },
+      ],
+    },
+    options: {
+      responsive: true,
+      scales: {
+        x: {
+          display: true,
+        },
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  });
+}
 
-
-export { createHydrationChart, createSleepChart };
+export { 
+  createHydrationChart, 
+  createSleepChart, 
+  // createActivityChart 
+};
